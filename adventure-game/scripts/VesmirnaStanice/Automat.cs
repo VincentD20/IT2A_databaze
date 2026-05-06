e86d27e9792f3019a1bd213a76f92e2e6b457118
Vincenzo Drápal <vincadrapa@gmail.com>
2026-05-06 22:27:38 +0200

    Add files via upload

---
+++ adventure-game/scripts/VesmirnaStanice/Automat.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Automat : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
+	{
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			VesmirnaStanice scene = GetParent<VesmirnaStanice>();
+			scene.ShowDialog("Našel jsi starou vesmírnou minci. Možná se hodí do automatů.");
+		}
+		// přidání mince do inventáře
+	}
+}
---
+++ adventure-game/scripts/VesmirnaStanice/FialDvere.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class FialDvere : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
+	{
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			VesmirnaStanice scene = GetParent<VesmirnaStanice>();
+			if(Inventory.HasItem("")) // item karta
+			{
+				GetTree().ChangeSceneToFile("res://scenes/VyskumnaLod.tscn");
+			}
+			scene.ShowDialog("Dveře jsou zamčené. Potřebuješ kartu.");
+
+		}
+	}
+}
